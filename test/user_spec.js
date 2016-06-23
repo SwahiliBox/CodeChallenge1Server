abd60916f143cb7072d205f30824db8dcaa3b1f1
Zachariah Ngonyani <zachariahn@gmail.com>
2016-06-23 21:05:18 +0300

    Add more test specs

--- test/user_spec.js
+++ test/user_spec.js
@@ -1,4 +1,13 @@
 describe("User", function(){
-  it("can log in", function(){
+  // just any other user 
+  it("has a name and email")
+  it("can log in")
+  it("can register by providing username, email and password")
+  it("can either be admin or normal user")
+
+  // context for admin users
+  describe("Admin User", function(){
+    it("Can create other users")
+    it("can delete and manager resources")
   })
 })
