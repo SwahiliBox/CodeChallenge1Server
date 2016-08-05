bd61a09be4dfa4598186753c1348bb3388e6ae88
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-05 12:58:09 +0300

    make code look pretty

--- app/models/event.js
+++ app/models/event.js
@@ -1,15 +1,15 @@
 //Schema for events display to users
-var mongoose = require('mongoose');
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
 
 //Create a Schema model to hold your events data.
-var eventSchema = mongoose.Schema({
+var EventSchema = Schema({
     title: String,
     venue: String,
     desc:  String,
     date:  {type :Date
-      //default: Date.now
     },
     time:  String,
 });
 
-module.exports = mongoose.model('Event',eventSchema);
+module.exports = mongoose.model('Event',EventSchema);
