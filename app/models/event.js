fae1b18d26f05412a6486ea31099aac94c3ec283
EmmanuelBeja <emmcodes@gmail.com>
2016-08-08 19:17:18 +0300

    event page to display a form for creating events

--- app/models/event.js
+++ app/models/event.js
@@ -3,12 +3,18 @@ var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
 
 //Create a Schema model to hold your events data.
-var EventSchema = Schema({
-    title: String,
-    venue: String,
-    desc:  String,
-    date:  {type: Date },
-    time:  String 
+var eventSchema = Schema({
+    meta: {
+      title: String,
+      venue: String,
+      desc:  String,
+      date:  {type: Date },
+      time:  String
+    }
 });
 
-module.exports = mongoose.model('Event',EventSchema);
+var Event = module.exports = mongoose.model('Event',eventSchema);
+module.exports.getEventByTitle = function(title, callback){
+  var query = {'meta.title': title};
+  Event.findOne(query,callback);
+}
