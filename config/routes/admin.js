a3c02854e29d3f984cbed69fcd32a9219447d5b6
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-30 18:46:04 +0300

    Add admin route

---
+++ config/routes/admin.js
@@ -0,0 +1,19 @@
+/*
+ * @admin routes 
+ * TODO: come up with other routes
+*/
+
+var Event = require('../../app/models/event');
+
+module.exports = {
+  index : function(req, res){
+    Event.find({}, function(error, events){
+      if(error) res.send(error);
+      res.render('events/index',{
+          events: events,
+          page: 'admin',
+          title: 'Admin Panel'
+      });
+    });
+  }
+};
