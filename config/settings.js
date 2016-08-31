71b5635cc454ab88447b0a83ccdda26e4d5e155f
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-31 12:11:19 +0300

    Add methods to dynamically get current environment's database and secret

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,18 @@
 var envs =  require('./database');
 
 module.exports = {
+
   getDB : function(environment){
+    var defaultUrl    =  'mongodb://localhost/nodetrials';
+    for(var i = 0; i < Object.keys(envs).length; i++){
+      return (i === environment)? envs+'.'+'environment'+'.'+url : defaultUrl;
+    }
+  },
+
+  getSecret : function(environment){
+    var defaultSecret =  'anystringoftext';
+    for(var i = 0; i < Object.keys(envs).length; i++){
+      return (i === environment)? envs+'.'+'environment'+'.'+secret : defaultSecret;
+    }
   }
-}
+};
