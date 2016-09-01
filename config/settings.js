1e5134ba8ff90a4aeb4371878bc6433175e5afac
Zachariah Ngonyani <zachariahn@gmail.com>
2016-09-01 14:00:03 +0300

    Make database used correspond to environment

--- config/settings.js
+++ config/settings.js
@@ -1,11 +1,12 @@
-var envs =  require('./database');
-var keys =  Object.keys(envs);
+var envs       =  require('./database');
+var keys       =  Object.keys(envs);
+var defaultDB  =  "mongodb://localhost/nodetrials";
 
-module.exports = {
+module.exports =  {
 
   getDB : function(environment){
     for(var i = 0; i < keys.length; i++){
-      return (i === environment)? envs+'.'+'environment'+'.'+url : process.env.MONGOLAB_URI;
+      return (i === environment)? envs+'.'+'environment'+'.'+url : defaultDB;
     }
   },
 
