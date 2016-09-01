afd7eb78628f92069e38db61e7a4514f8b826653
Zachariah Ngonyani <zachariahn@gmail.com>
2016-09-01 12:46:12 +0300

    set up the settings to get database from mongolab if nothing provided

--- config/settings.js
+++ config/settings.js
@@ -4,9 +4,8 @@ var keys =  Object.keys(envs);
 module.exports = {
 
   getDB : function(environment){
-    var defaultUrl    =  'mongodb://localhost/nodetrials';
     for(var i = 0; i < keys.length; i++){
-      return (i === environment)? envs+'.'+'environment'+'.'+url : defaultUrl;
+      return (i === environment)? envs+'.'+'environment'+'.'+url : process.env.MONGOLAB_URI;
     }
   },
 
