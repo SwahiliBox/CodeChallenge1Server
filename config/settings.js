d6b23287599b0b9b56a9cd7bcb44db58e8c41f58
Zachariah Ngonyani <zachariahn@gmail.com>
2016-09-01 15:13:13 +0300

    Fix code back to working version

--- config/settings.js
+++ config/settings.js
@@ -1,12 +1,11 @@
 var envs       =  require('./database');
 var keys       =  Object.keys(envs);
-var defaultDB  =  "mongodb://localhost/nodetrials";
 
 module.exports =  {
 
   getDB : function(environment){
     for(var i = 0; i < keys.length; i++){
-      return (i === environment)? envs+'.'+'environment'+'.'+url : defaultDB;
+      return (i === environment)? envs+'.'+'environment'+'.'+url : process.env.MONGOLAB_URI;
     }
   },
 
