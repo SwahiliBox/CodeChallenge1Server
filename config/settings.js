534a3aefce0f2032ac297803cf85f0703135543a
Zachariah Ngonyani <zachariahn@gmail.com>
2016-09-02 13:06:04 +0300

    Get the correct settings on how to get settings based on environment

--- config/settings.js
+++ config/settings.js
@@ -1,18 +1,19 @@
 var envs       =  require('./database');
-var keys       =  Object.keys(envs);
 
 module.exports =  {
 
   getDB : function(environment){
-    for(var i = 0; i < keys.length; i++){
-      return (i === environment)? envs+'.'+'environment'+'.'+url : process.env.MONGOLAB_URI;
+    for(i=0; i < envs.length;i++){
+      return (envs[i].environment === environment)? (envs[i].url) : process.env.MONGOLAB_URI;
     }
   },
 
   getSecret : function(environment){
+
     var defaultSecret =  'anystringoftext';
-    for(var i = 0; i < keys.length; i++){
-      return (i === environment)? envs+'.'+'environment'+'.'+secret : defaultSecret;
+
+    for(i=0; i < envs.length; i++){
+      return (envs[i].environment === environment)? (envs[i].secret) : defaultSecret;
     }
   }
 };
