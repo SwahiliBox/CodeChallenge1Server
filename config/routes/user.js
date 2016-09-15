363fb8c0ba90747c4da91f2ce52a309a3b11548b
Brian Muugweru <brianmugweru@gmail.com>
2016-09-15 23:22:00 +0300

    change user routes to send user details to phone app after creating a user

--- config/routes/user.js
+++ config/routes/user.js
@@ -1,4 +1,5 @@
 var User          = require('../../app/models/user');
+var Role          = require('../../app/models/role');
 
 module.exports    = {
   new : function(req, res){
@@ -9,36 +10,43 @@ module.exports    = {
   },
 
   create : function(req, res){
-    var user             =  new User();
-    user.local.firstname =  req.body.firstname;
-    user.local.surname   =  req.body.surname;
-    user.local.username  =  req.body.username;
-    user.local.email     =  req.body.email;
-    user.local.password  =  req.body.password;
-    user.local.picture   =  user.gravatar();
+    Role.findOne({ name:req.body.role}, function(err, role){
+      if(err) return(err);
 
-    User.getUserByUsername({username: req.body.username}, function(err, foundUser, done){
-      var message = 'That username is already taken';
-      if(err) throw err;
+      var user             =  new User();
+      user.local.firstname =  req.body.firstname;
+      user.local.surname   =  req.body.surname;
+      user.local.username  =  req.body.username;
+      user.local.email     =  req.body.email;
+      user.local.password  =  req.body.password;
+      user.local.picture   =  user.gravatar();
+      user.local.role      =  role._Id;
 
-      if(foundUser){
-        res.render('users/new', {
+      User.getUserByUsername({username: req.body.username}, function(err, foundUser, done){
+        var message = 'That username is already taken';
+        if(err) throw err;
+
+        if(foundUser){
+          res.render('users/new', {
             message: message
-        });
-      } else {
-        console.log('You have no register errors');
-        User.createUser(user,function(err, user){
-          if (err) throw err;
-
-          req.login(user, function(err){
-            if (!err){
-              res.redirect('/admin');
-            } else{
-              console.log("there was an error ", err);
-            }
           });
-        });
-      }
+        } else {
+          console.log('You have no register errors');
+          User.createUser(user,function(err, user){
+            if (err) throw err;
+
+            req.login(user, function(err){
+              if (!err){
+                console.log(user);
+                res.redirect('/admin');
+                //res.send(user);
+              } else{
+                console.log("there was an error ", err);
+              }
+            });
+          });
+        }
+      });
     });
   }
 };
