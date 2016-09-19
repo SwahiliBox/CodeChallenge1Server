9fd60d9c09e7e898aa2fa7e1392183c769882948
Brian Muugweru <brianmugweru@gmail.com>
2016-09-19 14:17:35 +0300

    copy paste route to get phone user sign up from phone application

--- config/routes/user.js
+++ config/routes/user.js
@@ -48,5 +48,45 @@ module.exports    = {
         }
       });
     });
+  },
+  phoneCreate : function(req, res){
+    Role.findOne({ name:req.body.role}, function(err, role){
+      if(err) return(err);
+
+      var user             =  new User();
+      user.local.firstname =  req.body.firstname;
+      user.local.surname   =  req.body.surname;
+      user.local.username  =  req.body.username;
+      user.local.email     =  req.body.email;
+      user.local.password  =  req.body.password;
+      user.local.picture   =  user.gravatar();
+      user.local.role      =  role._id;
+
+      User.getUserByUsername({username: req.body.username}, function(err, foundUser, done){
+        var message = 'That username is already taken';
+        if(err) throw err;
+
+        if(foundUser){
+          res.render('users/new', {
+            message: message
+          });
+        } else {
+          console.log('You have no register errors');
+          User.createUser(user,function(err, user){
+            if (err) throw err;
+
+            req.login(user, function(err){
+              if (!err){
+                console.log(user);
+                res.send(user);
+              } else{
+                console.log("there was an error ", err);
+              }
+            });
+          });
+        }
+      });
+    });
   }
+
 };
